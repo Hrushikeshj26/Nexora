ef5167f1ef5ba46207a1545a2e9cbfb4a2348834
Hrushikeshj26 <hrushij92@gmail.com>
2025-11-27 18:40:41 +0530

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Discover from './components/Discover'
+import Product from './pages/Product'
+import ScrollToTop from './components/ScrollToTop'
+import Offers from './components/Offer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-violet-200 min-h-screen flex flex-col">
+      
+      <Navbar />
+
+      {/* Main Content Wrapper */}
+      <main className="grow flex justify-center">
+        <div className="w-full max-w-7xl px-4">
+          <ScrollToTop />
+
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <Discover />
+                  <Offers />
+                </>
+              }
+            />
+            <Route path="/product/:id" element={<Product />} />
+          </Routes>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
   )
 }
 
